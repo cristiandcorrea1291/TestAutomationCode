18f4f4cebd61c6058d239d66a5d1d982191604d1
Dianayisney <dyisneydiaz@gmail.com>
2023-10-20 03:01:44 -0500

    Sesion 6 AdvancLocator

---
+++ Diana_Yisney_Diaz/Sesion_6/AdvancLocator/src/main/java/AdvancLocator/Locator8.java
@@ -0,0 +1,23 @@
+package AdvancLocator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Locator8 {
+	public static void main(String[] args)
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		//Sibling - Child to parennt traverse
+		//header/div/button[1]/following-sibling::button[1]
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+			System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+			System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+
+		
+	}
+}
